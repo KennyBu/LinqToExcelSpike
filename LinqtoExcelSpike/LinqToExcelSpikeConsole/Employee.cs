e53ff79bb8138c6b4531cec772cab4f1cc269e30
Ken Burkhardt <ken.burkhardt@gmail.com>
2014-01-09 12:49:39 -0500

    first pass

---
+++ LinqtoExcelSpike/LinqToExcelSpikeConsole/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LinqToExcelSpikeConsole
+{
+    public class Employee
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } 
+        public string Email { get; set; } 
+        public bool IsManager { get; set; } 
+        public DateTime StartDate { get; set; } 
+    }
+}
---
+++ LinqtoExcelSpike/LinqToExcelSpikeConsole/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LinqToExcel;
+
+namespace LinqToExcelSpikeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string excelPath = "c:/temp/excel/Employees.xlsx";
+
+            var excel = new ExcelQueryFactory(excelPath);
+            
+            var employees = from e in excel.Worksheet<Employee>("Employee")
+                                   select e;
+
+            Console.WriteLine("All Employees");
+            Console.WriteLine("");
+            WriteInfo(employees);
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            var managers = from e in excel.Worksheet<Employee>("Employee")
+                            where e.IsManager
+                            select e;
+
+            Console.WriteLine("Managers");
+            Console.WriteLine("");
+            WriteInfo(managers);
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            var regularEmployees = from e in excel.Worksheet<Employee>("Employee")
+                           where e.IsManager == false
+                           select e;
+
+            Console.WriteLine("Regular Employees");
+            Console.WriteLine("");
+            WriteInfo(regularEmployees);
+            Console.WriteLine("");
+            Console.Read();
+        }
+
+        private static void WriteInfo(IEnumerable<Employee> employees)
+        {
+            foreach (var employee in employees)
+            {
+                Console.WriteLine("Id:{0} Name:{1} Email:{2} IsManager:{3} StartDate{4}",
+                    employee.Id, employee.Name, employee.Email, employee.IsManager, employee.StartDate);
+            }
+        }
+    }
+}
